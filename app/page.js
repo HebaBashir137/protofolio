4817076ef4af91474071e4eafa43fd983fc28f01
HebaBashir137 <heba_4192@limu.edu.ly>
2026-05-19 17:12:47 +0300

    first portfolio setup

---
+++ app/page.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+
+      <section className="flex flex-col items-center justify-center h-screen text-center px-6">
+
+        <h1 className="text-6xl font-bold mb-6">
+          Hi, I’m YOUR NAME
+        </h1>
+
+        <p className="text-xl text-gray-400 max-w-2xl">
+          Hello, I’m Heba Gusibat, a software engineer specializing in building exceptional digital experiences. Welcome to my portfolio!
+          I'm Frontend Developer focused on building modern,
+          responsive, and user-friendly web applications.
+        </p>
+
+      </section>
+
+    </main>
+  );
+}
